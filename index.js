ccd7b55841dc1c38792ec323d5deb5cfd1e5d973
Sinod Kumar <sinodkr001@gmail.com>
2025-02-25 11:57:03 +0530

    update port

--- index.js
+++ index.js
@@ -457,7 +457,7 @@ app.post('/stop-scraping/:sessionId', (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist'))); 
 app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'dist', 'index.html')); });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
